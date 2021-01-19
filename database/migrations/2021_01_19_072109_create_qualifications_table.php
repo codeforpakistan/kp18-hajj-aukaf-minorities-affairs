92011a73e0e28a4e5b7e8115c63914a8bff9ab3d
Mubassir Hayat <mubassirhayat@gmail.com>
2021-01-19 20:17:41 +0500

    feat: qualifications migration

--- database/migrations/2021_01_19_072109_create_qualifications_table.php
+++ database/migrations/2021_01_19_072109_create_qualifications_table.php
@@ -15,6 +15,25 @@ class CreateQualificationsTable extends Migration
     {
         Schema::create('qualifications', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('applicant_id');
+            $table->foreignId('qualification_level_id');
+            $table->string('recent_class');
+            $table->string('current_class');
+            $table->foreignId('discipline_id');
+            $table->foreignId('institute_id');
+            $table->foreignId('degree_awarding_id');
+            $table->string('education_system');
+            $table->string('grading_system');
+            $table->double('total_cgpa', 3, 2);
+            $table->double('obtained_cgpa', 3, 2);
+            $table->integer('total_marks');
+            $table->integer('obtained_marks');
+            $table->double('percentage', 3, 2);
+            $table->date('passing_date');
+            $table->boolean('completed');            
+            $table->foreignId('created_by');
+            $table->foreignId('modified_by');
+            $table->foreignId('deleted_by');
             $table->softDeletes('deleted_at', 0);
             $table->timestamps();
         });
