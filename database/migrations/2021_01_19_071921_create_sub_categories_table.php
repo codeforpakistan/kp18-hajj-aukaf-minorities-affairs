fb057f2d2d2c64470bffa7361e1e447482775b72
Mubassir Hayat <mubassirhayat@gmail.com>
2021-01-19 12:38:48 +0500

    feat: sub_categories migration added

---
+++ database/migrations/2021_01_19_071921_create_sub_categories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sub_categories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('fund_category_id');
+            $table->string('type');
+            $table->text('description');
+            $table->boolean('status');
+            $table->softDeletes('deleted_at', 0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sub_categories');
+    }
+}
