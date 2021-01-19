cc870877a28bfd4ce2512bbf91fe1050c390fb69
Mubassir Hayat <mubassirhayat@gmail.com>
2021-01-19 21:11:57 +0500

    feat: qualification levels migration

--- database/migrations/2021_01_19_072115_create_qualification_levels_table.php
+++ database/migrations/2021_01_19_072115_create_qualification_levels_table.php
@@ -15,6 +15,8 @@ class CreateQualificationLevelsTable extends Migration
     {
         Schema::create('qualification_levels', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->foreignId('institute_type_id');
             $table->softDeletes('deleted_at', 0);
             $table->timestamps();
         });
