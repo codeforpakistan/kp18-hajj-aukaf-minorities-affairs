dc33c292007fd5543041d6b346b630c7039b17f9
Mubassir Hayat <mubassirhayat@gmail.com>
2021-01-19 21:12:37 +0500

    feat: institutes migration

--- database/migrations/2021_01_19_072210_create_institutes_table.php
+++ database/migrations/2021_01_19_072210_create_institutes_table.php
@@ -15,6 +15,17 @@ class CreateInstitutesTable extends Migration
     {
         Schema::create('institutes', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id');
+            $table->string('name');
+            $table->string('reg_num');
+            $table->boolean('affiliated_with_board');
+            $table->string('photo_of_affiliation');
+            $table->string('type');
+            $table->foreignId('institute_type_id');
+            $table->foreignId('city_id');
+            $table->string('institute_sector');
+            $table->text('address');
+            $table->string('contact_number');
             $table->softDeletes('deleted_at', 0);
             $table->timestamps();
         });
