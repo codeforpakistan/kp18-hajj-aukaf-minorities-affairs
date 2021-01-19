8748c09eaed859214ce3f827efa849dc452e2525
Mubassir Hayat <mubassirhayat@gmail.com>
2021-01-19 21:14:47 +0500

    feat: applicant migration

--- database/migrations/2021_01_19_072459_create_applicants_table.php
+++ database/migrations/2021_01_19_072459_create_applicants_table.php
@@ -15,6 +15,27 @@ class CreateApplicantsTable extends Migration
     {
         Schema::create('applicants', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('institute_class_id');
+            $table->string('name');
+            $table->string('father_name');
+            $table->string('husband_name');
+            $table->foreignId('religion_id');
+            $table->string('cnic');
+            $table->string('gender');
+            $table->string('domicile');
+            $table->foreignId('maritalstatus_id');
+            $table->string('gname');
+            $table->string('gfather_name');
+            $table->string('gcnic');
+            $table->string('gcontact');
+            $table->string('disease');
+            $table->string('dname');
+            $table->string('clinic_address');
+            $table->string('dcontact');
+            $table->string('image');
+            $table->string('operator_review');
+            $table->string('recommended_by');
             $table->softDeletes('deleted_at', 0);
             $table->timestamps();
         });
