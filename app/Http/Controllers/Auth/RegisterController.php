b044c9ee23c89d8eaa8b212658a6b84ec6038038
Mubassir Hayat <mubassirhayat@gmail.com>
2021-01-26 13:35:55 +0500

    feat: changes the way roles are assigned, and assigned the default role with role_id

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,12 +64,13 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-            'role_id' => 1,
         ]);
+        $user->assignRole('School');
+        return $user;
     }
 
     /**
